488a185978dd35d33539c04f409bbb3eda96c9f5
Jonny Schult <jonathon.schult@gmail.com>
2020-12-02 17:49:50 -0600

    API URL update

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch (window.location.hostname) {
   case "localhost" || "127.0.0.1":
     APIURL = "http://localhost:4040";
     break;
-  case "https://runnersheetsclient.herokuapp.com":
+  case "https://runnersheets.herokuapp.com":
     APIURL = "https://runnersheets.herokuapp.com";
 }
 
