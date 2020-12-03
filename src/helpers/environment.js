df18a5b41be5500acc6cd69ecb5b6ed575fdbece
Jonny Schult <jonathon.schult@gmail.com>
2020-12-02 20:24:34 -0600

    updated environment for new server name

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -5,7 +5,7 @@ switch (window.location.hostname) {
     APIURL = "http://localhost:4040";
     break;
   case "runnersheetsclient.herokuapp.com":
-    APIURL = "https://runnersheetsserver.herokuapp.com/";
+    APIURL = "https://runnersheetsserver.herokuapp.com";
 }
 
 export default APIURL;
