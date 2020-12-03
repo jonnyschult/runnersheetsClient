c39d625569b8f32a5b616fa2701e20973ed117b4
Jonny Schult <jonathon.schult@gmail.com>
2020-12-02 18:04:42 -0600

    tweaked environment to not include HTTPS://

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch (window.location.hostname) {
   case "localhost" || "127.0.0.1":
     APIURL = "http://localhost:4040";
     break;
-  case "https://runnersheets.herokuapp.com":
+  case "runnersheetsclient.herokuapp.com":
     APIURL = "https://runnersheets.herokuapp.com";
 }
 
