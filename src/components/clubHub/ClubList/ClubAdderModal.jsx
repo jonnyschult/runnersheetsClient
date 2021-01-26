a7751001b6e197e6e7617398f241a1f7fb129408
Jonny Schult <jonathon.schult@gmail.com>
2021-01-26 17:38:55 -0600

    Updated removal of clubs

--- src/components/clubHub/ClubList/ClubAdderModal.jsx
+++ src/components/clubHub/ClubList/ClubAdderModal.jsx
@@ -70,97 +70,6 @@ const ClubAdderModal = (props) => {
       });
   };
 
-  /************************
-  UPDATE CLUB
-  ************************/
-  const updateClub = (e) => {
-    e.preventDefault();
-    setLoading(true);
-    fetch(`${APIURL}/chairperson/updateClub`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: props.token,
-      },
-      body: JSON.stringify({
-        newClubName: clubTitle,
-        clubId: props.selectedClub.id,
-      }),
-    })
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        } else {
-          if (res.status === 409) {
-            throw new Error("Name Already Taken");
-          } else {
-            throw new Error("Something went wrong");
-          }
-        }
-      })
-      .then(async (data) => {
-        await props.setSelectedClub(data.updatedClub);
-        await props.setUpdate(data);
-        setLoading(false);
-        setResponse(data.message);
-        setTimeout(() => {
-          setResponse("");
-          toggle();
-        }, 1400);
-      })
-      .catch(async (err) => {
-        setLoading(false);
-        setErr(err.message);
-        setTimeout(() => {
-          setErr("");
-        }, 4000);
-      });
-  };
-
-  /************************
-  ONCLICK DELETE CLUB
-  ************************/
-  const deleteClub = (e) => {
-    e.preventDefault();
-    let confirmation = window.confirm(
-      "Are you certain you wish to delete this club?"
-    );
-    if (confirmation) {
-      setLoading(true);
-      fetch(`${APIURL}/chairperson/removeClub`, {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: props.token,
-        },
-        body: JSON.stringify({
-          clubId: props.selectedClub.id,
-        }),
-      })
-        .then((res) => res.json())
-        .then(async (data) => {
-          await props.setSelectedClub({});
-          await props.setUpdate(data);
-          await setResponse(data.message);
-          setLoading(false);
-          setTimeout(() => {
-            setResponse("");
-            toggle();
-          }, 1200);
-        })
-        .catch(async (err) => {
-          setLoading(false);
-          setErr(err.message);
-          setTimeout(() => {
-            setErr("");
-            toggle();
-          }, 4000);
-        });
-    } else {
-      toggle();
-    }
-  };
-
   return (
     <div>
       <Form inline onSubmit={(e) => e.preventDefault()}>
@@ -168,109 +77,50 @@ const ClubAdderModal = (props) => {
           +
         </Button>
       </Form>
-      {modalContent ? (
-        <Modal isOpen={modal} toggle={toggle} className={classes.modal}>
-          <ModalHeader toggle={toggle} className={classes.modalHeader}>
-            <header className={classes.headerText}>Update Club Name</header>
-          </ModalHeader>
-          <ModalBody className={classes.modalBody}>
-            <Form onSubmit={(e) => updateClub(e)} className={classes.form}>
-              <Label htmlFor="club name">
-                Rename or Delete Club <b>{props.selectedClub.clubName}</b>
-              </Label>
-              <Input
-                required
-                type="text"
-                name="club name"
-                defaultValue={props.selectedClub.clubName}
-                onChange={(e) => setClubTitle(e.target.value)}
-              ></Input>
-              <div className={classes.btnGroup}>
-                <Button
-                  className={`${classes.modalBtn} ${classes.updateBtn}`}
-                  type="submit"
-                >
-                  Update
-                </Button>
-                <Button
-                  className={`${classes.modalBtn} ${classes.deleteBtn}`}
-                  onClick={(e) => deleteClub(e)}
-                >
-                  Delete
-                </Button>
-              </div>
-              {loading ? <Spinner color="primary" /> : <></>}
-              {response ? (
-                <Alert className={classes.responseAlert}>{response}</Alert>
-              ) : (
-                <></>
-              )}
-              {err ? <Alert className={classes.errAlert}>{err}</Alert> : <></>}
-              <h6
-                className={classes.modalSwitch}
-                onClick={(e) => setModalContent(!modalContent)}
-              >
-                Create a New Club
-              </h6>
-            </Form>
-          </ModalBody>
-          <ModalFooter className={classes.modalFooter}>
+      <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle} className={classes.modalHeader}>
+          <header className={classes.headerText}>Create a Club!</header>
+        </ModalHeader>
+        <ModalBody className={classes.modalBody}>
+          <Form onSubmit={(e) => createClub(e)}>
+            <Label htmlFor="club name">Choose a Club Name</Label>
+            <Input
+              required
+              type="text"
+              name="club name"
+              placeholder="Chicago Ultra Runners"
+              onChange={(e) => setClubTitle(e.target.value)}
+            ></Input>
             <Button
-              className={`${classes.modalBtn} ${classes.cancelBtn}`}
-              onClick={toggle}
+              className={`${classes.modalBtn} ${classes.addBtn}`}
+              type="submit"
             >
-              Cancel
+              Create
             </Button>
-          </ModalFooter>
-        </Modal>
-      ) : (
-        <Modal isOpen={modal} toggle={toggle}>
-          <ModalHeader toggle={toggle} className={classes.modalHeader}>
-            <header className={classes.headerText}>
-              Create a Club!
-            </header>
-          </ModalHeader>
-          <ModalBody className={classes.modalBody}>
-            <Form onSubmit={(e) => createClub(e)}>
-              <Label htmlFor="club name">Choose a Club Name</Label>
-              <Input
-                required
-                type="text"
-                name="club name"
-                placeholder="Chicago Ultra Runners"
-                onChange={(e) => setClubTitle(e.target.value)}
-              ></Input>
-              <Button
-                className={`${classes.modalBtn} ${classes.addBtn}`}
-                type="submit"
-              >
-                Create
-              </Button>
-              {loading ? <Spinner color="primary" /> : <></>}
-              {response ? (
-                <Alert className={classes.responseAlert}>{response}</Alert>
-              ) : (
-                <></>
-              )}
-              {err ? <Alert className={classes.errAlert}>{err}</Alert> : <></>}
-              <h6
-                className={classes.modalSwitch}
-                onClick={(e) => setModalContent(!modalContent)}
-              >
-                Update
-              </h6>
-            </Form>
-          </ModalBody>
-          <ModalFooter className={classes.modalFooter}>
-            <Button
-              className={`${classes.modalBtn} ${classes.cancelBtn}`}
-              onClick={toggle}
+            {loading ? <Spinner color="primary" /> : <></>}
+            {response ? (
+              <Alert className={classes.responseAlert}>{response}</Alert>
+            ) : (
+              <></>
+            )}
+            {err ? <Alert className={classes.errAlert}>{err}</Alert> : <></>}
+            <h6
+              className={classes.modalSwitch}
+              onClick={(e) => setModalContent(!modalContent)}
             >
-              Cancel
-            </Button>
-          </ModalFooter>
-        </Modal>
-      )}
+              Update
+            </h6>
+          </Form>
+        </ModalBody>
+        <ModalFooter className={classes.modalFooter}>
+          <Button
+            className={`${classes.modalBtn} ${classes.cancelBtn}`}
+            onClick={toggle}
+          >
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 };
--- src/components/clubHub/ClubList/ClubList.jsx
+++ src/components/clubHub/ClubList/ClubList.jsx
@@ -1,4 +1,5 @@
 import ClubAdderModal from "./ClubAdderModal";
+import UpdateRemoveClubModal from "./UpdatRemoveClubModal";
 import classes from "../Club.module.css";
 import { Card, CardBody, CardTitle, CardText } from "reactstrap";
 
@@ -13,19 +14,27 @@ const ClubList = (props) => {
           {props.clubs.length > 0 ? (
             props.clubs.map((club, index) => {
               return (
-                <CardText
-                  className={classes.cardItem}
-                  key={index}
-                  onClick={async (e) => {
-                    await props.setChairpersons([]); //prevents error for ClubChairs  <i>{props.coachRole[index].role}</i>. Without, it would index too many times.
-                    props.fetchChairpersons(club.id);
-                    props.fetchAthletes(club.id);
-                    props.setSelectedClub(club);
-                    // props.setClubActivities([]);
-                  }}
-                >
-                  <b>{club.clubName}</b> <i>{club.role}</i>
-                </CardText>
+                <div className={`${classes.cardItem} ${classes.clubListItem}`}>
+                  <CardText
+                    className={`${classes.cardItem}`}
+                    key={index}
+                    onClick={async (e) => {
+                      await props.setChairpersons([]); //prevents error for ClubChairs  <i>{props.coachRole[index].role}</i>. Without, it would index too many times.
+                      props.fetchChairpersons(club.id);
+                      props.fetchAthletes(club.id);
+                      props.setSelectedClub(club);
+                      // props.setClubActivities([]);
+                    }}
+                  >
+                    <b>{club.clubName}</b> <i>{club.role}</i>
+                  </CardText>
+                  <UpdateRemoveClubModal
+                    club={club}
+                    token={props.token}
+                    setUpdate={props.setUpdate}
+                    setSelectedClub={props.setSelectedClub}
+                  />
+                </div>
               );
             })
           ) : (
